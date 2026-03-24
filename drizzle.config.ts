c510c72ad04c167700cfaaba9ddaa241d10a5484
Charles Bennett <bleudebennett@proton.me>
2026-03-24 19:18:57 +0100

    fix: small fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,16 @@
+import { env } from "@/env";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "src/schemas/*.ts",
   out: "drizzle",
   dialect: "postgresql",
+  dbCredentials: {
+    host: env.DATABASE_HOST,
+    port: env.DATABASE_PORT,
+    user: env.DATABASE_USER,
+    password: env.DATABASE_PASSWORD,
+    database: env.DATABASE_NAME,
+    ssl: { rejectUnauthorized: false },
+  },
 });
